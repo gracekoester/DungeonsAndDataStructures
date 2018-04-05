6c881b51474991975ef7d92f5285a48bab9ba684
Grace <gmk1914@yahoo.com>
2018-04-04 22:00:07 -0500

    //added new exit

--- app/src/main/java/com/example/awesomefat/dungeonsanddatastructures/Exit.java
+++ app/src/main/java/com/example/awesomefat/dungeonsanddatastructures/Exit.java
@@ -18,5 +18,19 @@ public class Exit
     public void takeExit(Player p)
     {
         //make the player move to the room they are NOT currently in.
+
+        if(p.getCurrentRoom() != this.r1)
+        {
+            p.setCurrentRoom(this.r1);
+        }
+        else (p.getCurrentRoom() != this.r2)
+        {
+            p.setCurrentRoom(this.r2);
+        }
+
+        System.out.println("Character " + p + "is now in room " + p.getCurrentRoom());
+
+
+    }
     }
 }
